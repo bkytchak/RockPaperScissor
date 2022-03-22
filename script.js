473e11e5691020c55450ffcbb590d76cf070b1de
Brent A Kytchak <bkytchak@gmail.com>
2022-03-22 14:44:28 -0400

    Update script.js
    Added computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    const randomNum = (Math.floor(Math.random()*choices.length));
+    return choices[randomNum];
+}
 
+console.log(computerPlay());

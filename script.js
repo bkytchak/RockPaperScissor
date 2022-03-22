1e3fad6c755f56fb73a60f546b81b1cc33707235
Brent A Kytchak <bkytchak@gmail.com>
2022-03-22 15:58:58 -0400

    Update script.js
    Created roundPlay function

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     const randomNum = (Math.floor(Math.random()*choices.length));
     return choices[randomNum];
+    
 }
 
-console.log(computerPlay());
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return"Tie. Play Again";
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+        return "You win. Rock beats scissors";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
+        return "You win. Scissors beats paper";
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You win. Paper beats rock";
+    } else {
+        return "You lose. Play again.";
+    }
+    
+    
+
+}
+
+const computerSelection = computerPlay();
+const playerSelection  = "paper"
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));

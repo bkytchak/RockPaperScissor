b26600936cddfdb402a6b668c6e22e6e9808d79f
Brent A Kytchak <bkytchak@gmail.com>
2022-03-24 15:16:45 -0400

    Update script.js
    Added game function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 function computerPlay() {
     let choices = ["rock", "paper", "scissors"];
     const randomNum = (Math.floor(Math.random()*choices.length));
@@ -5,24 +8,41 @@ function computerPlay() {
     
 }
 
+//Plays a single round of the game
 function playRound (playerSelection, computerSelection) {
+    let result = " "
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return"Tie. Play Again";
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        return "You win. Rock beats scissors";
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper"){
-        return "You win. Scissors beats paper";
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        return "You win. Paper beats rock";
-    } else {
-        return "You lose. Play again.";
-    }
-    
-    
+        result =  ("Tie. Play again.")
+    } else if ((playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") ||
+            (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")  ||
+            (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock")) {
 
+            playerScore++
+            result = ("You win. " + playerSelection + " beats " + computerSelection + ".")
+    } else {
+        computerScore++
+        result = ("You lose. " + computerSelection + " beats " + playerSelection + ".")
+    }
+return result
 }
 
-const computerSelection = computerPlay();
-const playerSelection  = "paper"
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let roundResult = ""
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let computerSelection = computerPlay()
+        console.log(playRound(playerSelection, computerSelection))
+        console.log("your score = " + playerScore);
+        console.log("Computer's score = " + computerScore);
+    } 
+    if (playerScore < computerScore){
+        roundResult =  "Loser"
+    } else if (playerScore > computerScore){
+        roundResult = "You win"
+    } else {
+        roundResult = "Tie. Refresh page to play again."
+    }
+    return roundResult
+}
+
+console.log(game())
